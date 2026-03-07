76f2f7b3131b8709b6d5a7b9086581533f93fe17
idir <idirb2005@gmail.com>
2026-03-07 14:11:07 +0000

    working on uploadmiddleware

--- Backend/controllers/authcontrollers.js
+++ Backend/controllers/authcontrollers.js
@@ -63,11 +63,16 @@ export async function loginUser(req,res) {
 }
 
 export async function getUserInfo(req,res) {
-    const user = await user.findById(req.user.id).select("-password");
+    try{
+        const user = await User.findById(req.user.id).select("-password");
 
-    if (!user) {
-        return res.status(404).json({message: "user not found"})
+        if (!user) {
+            return res.status(404).json({message: "user not found"})
+        }
+
+        res.status(200).json(user);
+    }catch(error){
+        res.status(500).json({message: "error finding user ", error : error.message})
+
     }
-
-    res.status(200).json(user);
 }
--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -1,11 +1,13 @@
 import express from 'express'
 import { registerUser, loginUser, getUserInfo} from '../controllers/authcontrollers.js'
+import {protect} from '../middleware/authmiddleware.js'
 
 const router = express.Router();
 
 router.post("/register", registerUser);
+
 router.post("/login", loginUser);
-/* 
+
 router.get("/getuser",protect ,getUserInfo);
- */
+ 
 export default router
